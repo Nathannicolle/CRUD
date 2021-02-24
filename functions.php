174ead73f603ed3d371ea3b310b1f4157e70dea2
Nathannicolle <nathan-nicolle@orange.fr>
2021-02-24 15:06:21 +0100

    ajout des fichiers de base du CRUD

---
+++ functions.php
@@ -0,0 +1,19 @@
+<?php 
+function get_header() {
+?> 
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <title>CRUD</title>
+    </head>
+    <body>
+<?php
+}
+
+function get_footer() {
+?>
+    </body>
+</html>
+<?php
+} ?>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+include "functions.php";
+get_header();
+?>
+<h1>Bienvenue sur cette interface de crud</h1>
+<ul>
+    <li><a href="#">Créer une organisation</a></li>
+    <li><a href="#">Voir/Editer les organisations</a></li>
+</ul>
