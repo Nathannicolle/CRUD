e3f07422d09ecc6751dad089194711955f218d33
Nathannicolle <nathan-nicolle@orange.fr>
2021-02-25 18:35:37 +0100

    Ajout de la liste des organisations
    Ajout de la liste des organisations + du champ relatif aux aliaises

--- index.php
+++ index.php
@@ -5,5 +5,5 @@ get_header();
 <h1>Bienvenue sur cette interface de crud</h1>
 <ul>
     <li><a href="scripts/create.php">Créer une organisation</a></li>
-    <li><a href="#">Voir/Editer les organisations</a></li>
+    <li><a href="scripts/database_view.php">Voir/Editer les organisations</a></li>
 </ul>
