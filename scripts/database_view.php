af14adfd5c0485015e6fd088d8d85dec5529a24a
Nathannicolle <nathan-nicolle@orange.fr>
2021-03-03 10:52:16 +0100

    ajout de font awesome

--- scripts/database_view.php
+++ scripts/database_view.php
@@ -11,9 +11,12 @@ $statement=connect()->query('select * from Organization');
 </tr>
 
 <?php
+$idligne = 0;
 foreach ($statement as $row) {
     echo "<tr><td>" . $row['name'] . "</td>";
     echo "<td>" . $row['domain'] . "</td>";
-    echo "<td>" . $row['aliases'] . "<br></td></tr>";
+    echo "<td>" . $row['aliases'] . "<br></td>";
+    $idligne = $idligne + 1;
+    echo "<td><button type='submit' class='fas fa-trash-alt button_delete'></button></td></tr>";
 }?>
 </table>
--- scripts/functions.php
+++ scripts/functions.php
@@ -6,6 +6,7 @@ function get_header() {
     <head>
         <meta charset="utf-8">
         <title>CRUD</title>
+        <link rel="stylesheet" type="text/css" href="../assets/fa/css/all.css">
         <link rel="stylesheet" type="text/css" href="../assets/css/style.css">
     </head>
     <body>
